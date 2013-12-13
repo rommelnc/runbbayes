970a399b00ec7c8487b92caeb1a16548273b3980
Samuel Pala <samuelpala@gmail.com>
2013-12-13 17:49:23 -0200

    a

--- teste_commit.R
+++ teste_commit.R
@@ -8,6 +8,21 @@ samuel
 
 fernando
 
+suspeito = createNodeInfo(~suspeito, c(0.5, 0.5), c("yes","no"))
+feriado = createNodeInfo(~feriado, c(0.5, 0.5), c("yes", "no"))
+alcool = createNodeInfo(~alcool|suspeito:feriado, c(0.2,0.8,0.8,0.2,0.5, 0.5,0.5, 0.5), c("consumiu","Nconsumiu"))
+teste = createNodeInfo(~teste|alcool, c(0.99, 0.01, 0.001, 0.999), c("positivo", "negativo"))
+
+nodeList = list(suspeito, feriado, alcool, teste)
+
+network = createNetwork(nodeList, compile=TRUE)
+
+netCompiled = compileNetwork(network)
+
+io = .jnew("unbbayes/io/NetIO")
+file = .jnew("java/io/File", "C:/Users/Samuel Pala/Desktop/teste.net")
+io$save(file, network$network)
+
 node.a = createNodeInfo(~asia, prob=c(0.01, 0.99), states=c("yes","no"))
 node.t = createNodeInfo(~tub|asia, prob=c(0.05, 0.95, 0.01, 0.99),states=c("yes","no"))
 node.s = createNodeInfo(~smoke, prob=c(0.5,0.5), states=c("yes","no"))
@@ -22,3 +37,7 @@ nodes = list(node.a, node.t, node.s, node.l, node.b, node.e, node.x, node.d)
 net = createNetwork(nodes)
 
 netCompiled = compileNetwork(net)
+
+io = .jnew("unbbayes/io/NetIO")
+file = .jnew("java/io/File", "C:/Users/Samuel Pala/Desktop/teste.net")
+io$save(file, net$network)
